ccd9966b6a1440535848624c81036e9571fa4e1f
Zekun <zekunl@usc.edu>
2022-11-23 15:14:23 -0600

    clean up

--- test_template.py
+++ test_template.py
@@ -9,7 +9,8 @@ from statistics import mode
 from test_dnn import write_output, write_to_tif
 from const_test import * #template_matching_indices
 from imutils.object_detection import non_max_suppression
-from utils.ModifiedTM_cc import *
+from kneed import KneeLocator
+import math
 
 IF_DEBUG = False
 
@@ -22,7 +23,80 @@ else:
 
 
 
+def adjust_elbow(elbow):
+    thresh = math.ceil(elbow*100)/100 
+    if thresh < 0.95:
+        thresh += 0.02
+    return thresh
 
+def template_matching(img, temp_img, method):
+
+    # find all the template matches in the basemap
+    res1 = cv2.matchTemplate(img[:,:,0], temp_img[:,:,0],method ) #  #cv2.TM_CCOEFF_NORMED)
+    res2 = cv2.matchTemplate(img[:,:,1], temp_img[:,:,1],method ) # eval(methods[-1])) #cv2.TM_CCOEFF_NORMED)
+    res3 = cv2.matchTemplate(img[:,:,2], temp_img[:,:,2],method ) # eval(methods[-1])) #cv2.TM_CCOEFF_NORMED)
+
+    histogram, bin_edges = np.histogram(res1, bins=256, range=(0.0, 1.0))
+    
+    kn = KneeLocator(bin_edges[np.argmax(histogram)+1:], histogram[np.argmax(histogram):], curve='convex', direction='decreasing')
+    print(kn.knee)
+    elbow0 = kn.knee
+    
+
+    # fig, ax = plt.subplots()
+    # plt.plot(bin_edges[0:-1], histogram)
+    # plt.title("Grayscale Histogram")
+    # plt.xlabel("grayscale value")
+    # plt.ylabel("pixels")
+    # plt.xlim(0, 1.0)
+    
+    histogram, bin_edges = np.histogram(res2, bins=256, range=(0.0, 1.0))
+    
+    kn = KneeLocator(bin_edges[np.argmax(histogram)+1:], histogram[np.argmax(histogram):], curve='convex', direction='decreasing')
+    elbow1 = kn.knee
+
+
+    # fig, ax = plt.subplots()
+    # plt.plot(bin_edges[0:-1], histogram)
+    # plt.title("Grayscale Histogram")
+    # plt.xlabel("grayscale value")
+    # plt.ylabel("pixels")
+    # plt.xlim(0, 1.0)
+    
+    histogram, bin_edges = np.histogram(res3, bins=256, range=(0.0, 1.0))
+    
+    kn = KneeLocator(bin_edges[np.argmax(histogram)+1:], histogram[np.argmax(histogram):], curve='convex', direction='decreasing')
+    elbow2 = kn.knee
+
+    
+    # fig, ax = plt.subplots()
+    # plt.plot(bin_edges[0:-1], histogram)
+    # plt.title("Grayscale Histogram")
+    # plt.xlabel("grayscale value")
+    # plt.ylabel("pixels")
+    # plt.xlim(0, 1.0)
+    
+    # elbow = np.max([elbow1,elbow2,elbow0])
+    elbow0 = adjust_elbow(elbow0)
+    elbow1 = adjust_elbow(elbow1)
+    elbow2 = adjust_elbow(elbow2)
+    
+    
+    print(elbow0, elbow1,elbow2)
+    
+
+    res = np.bitwise_and(np.bitwise_and(res1>elbow0,res2>elbow1),res3>elbow2)
+    print(np.min(res1),np.max(res1))
+    print(np.min(res2),np.max(res2))
+    print(np.min(res3),np.max(res3))
+    return res
+
+def find_contours(mask):
+    ret, thresh = cv2.threshold(mask, 200, 255, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+
+        
 def get_files_in_folder(point_folder):
     img_path_list = glob.glob(point_folder + '/*_pt.jpeg')
     img_path_list = sorted(img_path_list)
@@ -43,7 +117,6 @@ def highlight_block(start_h, end_h, start_w, end_w, img, fill_value = 255):
     img[start_h:end_h, start_w:end_w] = fill_value
     return img
 
-
 print('template matching indices',template_matching_indices)
 
 val_img_path_list = get_files_in_folder(point_validation_folder)
@@ -58,7 +131,6 @@ for idx in template_matching_indices:
     out_file_path = os.path.join(out_dir, img_name + '_' + label_name + '.tif')
     
     
-
     img_path = os.path.join(input_img_dir, img_name + '.tif')
     img = cv2.imread(img_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -69,13 +141,20 @@ for idx in template_matching_indices:
     
     th, tw = temp_img.shape[0], temp_img.shape[1]
 
-    points_list = modifiedMatchTemplate(img, temp_img, "TM_CCORR_NORMED", 0.8, 500, [0,360], 10, [100,110], 10, True, True) 
-    print(len(points_list))
+    res = template_matching(img, temp_img, cv2.TM_CCORR_NORMED)
 
+    res = res.astype(np.uint8) * 255
+    print(np.unique(res))
+    
+    
+    contours = find_contours(res)
+    num_detects = len(contours)
+    print(num_detects)
     pred_img = np.zeros((img.shape[0],img.shape[1])).astype(np.uint8)
-    for pred in points_list:
-        coord = pred[0]
-        c_w, c_h = coord
+    
+    for contour in contours:
+        c_w = int(np.mean(contour[:,:,0]))
+        c_h = int(np.mean(contour[:,:,1]))
 
         if IF_DEBUG:
             pred_img = highlight_block(c_h, c_h+th, c_w, c_w+tw, pred_img, fill_value = 255)
